87266aa402d82499ce9ca968bb8e8c3908cf29e2
NikitaShulgan <80168174+NikitaShulgan@users.noreply.github.com>
2021-03-22 21:48:16 +0300

    Update train.py

--- train.py
+++ train.py
@@ -75,7 +75,7 @@ def build_model():
 
 def step_decay(epoch):
    initial_lrate = 0.1
-   drop = 0.5
+   drop = 0.6
    epochs_drop = 3.0
    lrate = initial_lrate * math.pow(drop,  
            math.floor((1+epoch)/epochs_drop))
