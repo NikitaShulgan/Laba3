1c29f3423c9a7eca3416e2f03122a51c61559094
NikitaShulgan <80168174+NikitaShulgan@users.noreply.github.com>
2021-03-22 19:30:48 +0300

    Update train.py

--- train.py
+++ train.py
@@ -97,7 +97,7 @@ def main():
   log_dir='{}/owl-{}'.format(LOG_DIR, time.time())
   model.fit(
     train_dataset,
-    epochs=150,
+    epochs=200,
     validation_data=validation_dataset,
     callbacks=[
       tf.keras.callbacks.TensorBoard(log_dir),
